d785fbf4aa50aec50f51e6b459dc80da83369dfb
Justin <justinbfigueroa@gmail.com>
2022-08-19 17:38:22 -0700

    Add fixes to remove both profile and plugin blocks

--- src/main/java/net/civserver/md5remover/Main.java
+++ src/main/java/net/civserver/md5remover/Main.java
@@ -1,20 +1,24 @@
 package net.civserver.md5remover;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static final String OPENING_BLOCK = "<plugin>";
+    public static final String OPENING_PLUGIN_BLOCK = "<plugin>";
+    public static final String CLOSING_PLUGIN_BLOCK = "</plugin>";
+    public static final String OPENING_PROFILE_BLOCK = "<profile>";
+    public static final String CLOSING_PROFILE_BLOCK = "</profile>";
     public static final String PLUGIN_NAME = "specialsource-maven-plugin";
-    public static final String CLOSING_BLOCK = "</plugin>";
 
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -29,55 +33,40 @@ public class Main {
                 System.exit(-1);
             }
 
-            Scanner in = new Scanner(f);
+            Scanner sc = new Scanner(f);
 
             List<String> lines = new ArrayList<>();
-            int upperIndex = -1;
             int targetIndex = -1;
-            int lowerIndex = -1;
-            while (in.hasNextLine()) {
-                String l = in.nextLine();
+            while (sc.hasNextLine()) {
+                String l = sc.nextLine();
                 // Only look for the first instance of the plugin
                 if (targetIndex == -1 && l.contains(PLUGIN_NAME)) {
                     targetIndex = lines.size(); // we found the index of the specialsource plugin
                 }
                 lines.add(l);
             }
-            in.close();
+            sc.close();
 
             if (targetIndex == -1) {
                 System.out.println("We did not find the specialsource plugin in this file.");
                 System.exit(-1);
             }
 
-            for (int i = targetIndex; i >= 0; i--) {
-                if (lines.get(i).contains(OPENING_BLOCK)) {
-                    upperIndex = i;
-                    break;
-                }
-            }
+            // Create a backup file
+            InputStream in = new BufferedInputStream(new FileInputStream(f));
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(new File(f.getParent(), f.getName() + ".bak")));
 
-            if (upperIndex == -1) {
-                System.out.println("We did not find the specialsource plugin in this file.");
-                System.exit(-1);
+            byte[] buffer = new byte[1024];
+            int lengthRead;
+            while ((lengthRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, lengthRead);
+                out.flush();
             }
+            in.close();
+            out.close();
 
-            for (int i = targetIndex; i < lines.size(); i++) {
-                if (lines.get(i).contains(CLOSING_BLOCK)) {
-                    lowerIndex = i;
-                    break;
-                }
-            }
-
-            if (lowerIndex == -1) {
-                System.out.println("We did not find the specialsource plugin in this file.");
-                System.exit(-1);
-            }
-
-            // Cleanse the file of the plugin block
-            for (int i = 0; i < lowerIndex - upperIndex; i++) {
-                System.out.println("Removed line: " + lines.remove(upperIndex));
-            }
+            removeBlock(lines, PLUGIN_NAME, OPENING_PLUGIN_BLOCK, CLOSING_PLUGIN_BLOCK);
+            removeBlock(lines, PLUGIN_NAME, OPENING_PROFILE_BLOCK, CLOSING_PROFILE_BLOCK);
 
             FileWriter fw = new FileWriter(f, false);
             for (String s : lines) {
@@ -86,6 +75,53 @@ public class Main {
             fw.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void removeBlock(List<String> lines, String target, String opening, String closing) {
+        int upperIndex = -1;
+        int targetIndex = -1;
+        int lowerIndex = -1;
+
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).contains(target)) {
+                targetIndex = i;
+                break;
+            }
+        }
+
+        if (targetIndex == -1) {
+            System.out.println("Found plugin name, but invalid block!");
+            return;
+        }
+
+        for (int i = targetIndex; i >= 0; i--) {
+            if (lines.get(i).contains(opening)) {
+                upperIndex = i;
+                break;
+            }
+        }
+
+        if (upperIndex == -1) {
+            System.out.println("Found plugin name, but invalid block!");
+            return;
+        }
+
+        for (int i = targetIndex; i < lines.size(); i++) {
+            if (lines.get(i).contains(closing)) {
+                lowerIndex = i;
+                break;
+            }
+        }
+
+        if (lowerIndex == -1) {
+            System.out.println("Found plugin name, but invalid block!");
+            return;
+        }
+
+        // Cleanse the file of the plugin block
+        for (int i = 0; i < lowerIndex - upperIndex + 1; i++) {
+            System.out.println("Removed line: " + lines.remove(upperIndex));
         }
     }
 }
